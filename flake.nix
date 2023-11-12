9739bcdc1f10a2384524a9312f28a5de0627e7a0
Ciaran De Groot <ciaran.deg@gmail.com>
2023-11-11 18:33:59 -0700

    Add theme to portfolio flake package

--- flake.nix
+++ flake.nix
@@ -8,13 +8,22 @@
     packages.${system} = let
       pkgs = nixpkgs.legacyPackages.${system};
     in rec {
+      hugo-theme-anubis = pkgs.stdenv.mkDerivation {
+        name = "hugo-theme-anubis";
+        src = self.inputs.hugo-theme-anubis;
+        buildPhase = ''
+          mkdir -p $out/share
+          cp -r ./* $out/share/
+        '';
+      };
       portfolio = pkgs.stdenv.mkDerivation {
         name = "portfolio";
         src = ./.;
         buildPhase = ''
           ${pkgs.hugo}/bin/hugo
-          mkdir -p $out
-          mv public $out/public
+          mkdir -p $out/themes
+          cp -r public $out/public
+          cp -r ${hugo-theme-anubis}/share $out/public/themes/hugo-theme-anubis
         '';
       };
       default = portfolio;
