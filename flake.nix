c52d25d0f0a7dfff44b59309d8b25214541db374
Ciaran De Groot <ciaran.deg@gmail.com>
2026-03-08 14:27:59 -0300

    Move flake and static content to root

--- flake.nix
+++ flake.nix
@@ -12,38 +12,11 @@
     lib = nixpkgs.lib;
   in {
     packages.${system} = {
-      # `nix build .#public` generates the contents of a static site
-      public = pkgs.stdenv.mkDerivation {
-        pname = "ciarandg-portfolio";
-        version = ""; # unversioned
-        src = ./.;
-        nativeBuildInputs = [pkgs.hugo];
-        buildPhase = "hugo";
-        installPhase = ''
-          mkdir -p $out
-          cp -r ./public/* $out/
-        '';
-      };
-
-      # `nix run .#hugo` lets you access the same version of hugo used in this flake
-      hugo = pkgs.hugo;
-
       # `nix run .#s3cmd` lets you access the same version of s3cmd used in this flake
       s3cmd = pkgs.s3cmd;
-
-      default = self.packages.${system}.public;
     };
 
     apps.${system} = {
-      # `nix run .#dev` runs a development server with hot reload and drafts enabled
-      dev = {
-        type = "app";
-        program = lib.getExe (pkgs.writeShellScriptBin "dev" ''
-          ${lib.getExe pkgs.hugo} serve -D
-        '');
-      };
-      develop = self.apps.${system}.dev;
-
       # `nix run .#pull-assets` downloads all static content from object storage to ./static/
       pull-assets = {
         type = "app";
@@ -56,7 +29,7 @@
       push-assets = {
         type = "app";
         program = lib.getExe (pkgs.writeShellScriptBin "push-assets" ''
-            ${lib.getExe pkgs.s3cmd} sync --acl-public ./static/ s3://ciarandg-portfolio/
+          ${lib.getExe pkgs.s3cmd} sync --acl-public ./static/ s3://ciarandg-portfolio/
         '');
       };
     };
